8119794c528aa4b66e3fbd46b3751b21bd3133a3
Alex Warren <alexander.ch.warren@gmail.com>
2024-10-01 15:31:51 -0400

    Add YAML functionality

--- replace-template.py
+++ replace-template.py
@@ -1,6 +1,26 @@
 import argparse
+from pathlib import Path
+
+import yaml
+
+
+def load_replacements(config_file: str = "./replacements.yaml"):
+    with open(config_file) as stream:
+        try:
+            print(yaml.safe_load(stream))
+        except yaml.YAMLError as e:
+            print(e)
+
+
+def replace_keywords(path: Path, replacements: dict[str, str]):
+    pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--project-path")
 
+    args = parser.parse_args()
+    print(args)
+
+    load_replacements()
