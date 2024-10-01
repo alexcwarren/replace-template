511bc3f52805d9187fe684a6caaf4fa5e594b243
Alex Warren <alexander.ch.warren@gmail.com>
2024-10-01 18:13:25 -0400

    Implement more of test Directory; Add test_load_replacements()

--- tests/test_replace_template.py
+++ tests/test_replace_template.py
@@ -1,32 +1,54 @@
+import random
 import shutil
 from pathlib import Path
 
 import pytest
 
-# import replace_template as rt
+import replace_template as rt
 
 
-class TestDirectory:
-    def __init__(self, parent_dir: str):
+class Directory:
+    def __init__(
+        self,
+        parent_dir: str,
+        max_num_folders: int = 10,
+        max_num_files: int = 10,
+        seed: int = 1234,
+    ):
         parent_path: Path = Path(parent_dir)
+
         test_dir: str = "test_dir"
         self.__test_path: Path = parent_path.joinpath(test_dir)
+
         if self.__test_path.exists():
             self.remove_all()
+
         self.__test_path.mkdir(parents=True)
+
+        self.__create_contents()
+
+    def __create_contents(self):
+        """TODO"""
+        pass
+        # Create 1 to max number of folders.
+
+        # In each folder, create 1 to max number of files.
+
+        # In each file, populate with random text intersparsed with at least 1 of each
+        # keyword.
 
     def remove_all(self):
         shutil.rmtree(self.__test_path)
 
 
 @pytest.fixture
-def test_dir(request) -> TestDirectory:
+def test_dir(request) -> Directory:
     """TODO"""
     # Get command-line arg value of variable(s) defined in conftest.py
     keep_test_dir: bool = request.config.getoption("--keep-test-dir")
 
     # Create test directory and its contents
-    testdir: TestDirectory = TestDirectory(Path.cwd())
+    testdir: Directory = Directory(Path.cwd())
 
     # Create nested function to call upon pytest conclusion
     def finalizer():
@@ -38,6 +60,13 @@ def test_dir(request) -> TestDirectory:
     request.addfinalizer(finalizer)
 
     return testdir
+
+
+def test_load_replacements():
+    """TODO"""
+    dir_level: str = ".." if Path.cwd().name == "tests" else "."
+    replacements: dict = rt.load_replacements(f"{dir_level}/replacements.yaml")
+    print(replacements)
 
 
 def test_replace_template(test_dir):
