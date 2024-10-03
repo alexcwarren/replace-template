b76ac2a3cbc545070354693c7a4e18ca5d9cc379
Alex Warren <alexander.ch.warren@gmail.com>
2024-10-03 15:52:45 -0400

    Add seed and replacements CL args; Add path fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,20 @@
-def pytest_addoption(parser):
+import pytest
+
+
+def pytest_addoption(parser: pytest.Parser):
+    """TODO"""
     parser.addoption("--keep-test-dir", action="store_true")
+    parser.addoption("--seed")
+    parser.addoption("--replacements-file", default="replacements.yaml")
+
+
+@pytest.fixture
+def project_path(request: pytest.FixtureRequest) -> str:
+    """TODO"""
+    return str(request.config.rootpath)
+
+
+@pytest.fixture
+def config_path(request: pytest.FixtureRequest, project_path: str) -> str:
+    """TODO"""
+    return f"{project_path}\\{request.config.getoption('--replacements-file')}"
